af43c5b74ec3cb103865538da5035f91c89108b4
fabioacordeiro <126525817+fabioacordeiro@users.noreply.github.com>
2023-08-21 22:49:13 -0300

    ok
    atualizando

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,9 +6,12 @@ class AlienInvasion:
     def __init__(self):
         'Ininializa o jogo e crie recursos do jogo'
         pygame.init()
+        self.clock=pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
+        #Deine a cor do background
+        self.bg_color=(230,230,230)
 
     def run_game(self):
         "Inicia o loop principal do jogo"
@@ -17,8 +20,11 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            #Redesenha a tela durante cada passagem pelo loop
+            self.screen.fill(self.bg_color)
             #Deixa a tela desenhada mais recente visivel
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 
